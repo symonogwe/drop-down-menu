39ef022f2c0e8be4af6eddd72291c8ce8a7be312
Symon Ogwe <symonogwe@gmail.com>
2023-09-18 23:33:21 +0300

    Create nav bar elements & add style to them

--- src/nav.js
+++ src/nav.js
@@ -1,6 +1,9 @@
 import logoSvg from "./Assets/logo.svg";
+import dropUpSvg from "./Assets/drop-up.svg";
+import dropDownSvg from "./Assets/drop-down.svg";
 
-const logoImg = document.querySelector(".logo-img");
+export const logoImg = document.querySelector(".logo-img");
 logoImg.src = logoSvg;
 
-export default logoImg;
+export const menuArrow = document.querySelector(".menu-arrow");
+menuArrow.src = dropUpSvg;
