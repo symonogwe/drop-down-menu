b91194930eb5d0dcd3d38acdc817fb3ff663e3e2
Symon Ogwe <symonogwe@gmail.com>
2023-09-18 22:18:41 +0300

    Create nav.js module
    - Add drop down svg's to the Assets folder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log("Hello World!");
+import * as navComponents from "./nav";
---
+++ src/nav.js
@@ -0,0 +1,6 @@
+import logoSvg from "./Assets/logo.svg";
+
+const logoImg = document.querySelector(".logo-img");
+logoImg.src = logoSvg;
+
+export default logoImg;
