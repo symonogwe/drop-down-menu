7240ccb29ac4ef01e99c4b545f450ad3a7a98f61
Symon Ogwe <symonogwe@gmail.com>
2023-09-19 21:26:00 +0300

    Create functioning dropDown menu

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 import "./styles/styles.css";
 
-// import toggleMenuBtn from "./nav";
-
-const toggleMenuBtn = document.querySelectorAll(".toggle-menu");
-
-function toggleDropDown() {
-  const dropDown = document.querySelector(".nav-links-container");
-  const caret = document.querySelector(".caret");
-
-  dropDown.classList.toggle(".visible-menu");
-  caret.classList.toggle("caret-rotate");
-}
-
-toggleMenuBtn.forEach((btn) => {
-  btn.addEventListener("click", toggleDropDown);
-});
+import * as navComponents from "./nav";
