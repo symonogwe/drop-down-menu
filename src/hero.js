9129286079ce857646cc1b876709f7e375f15e37
Symon Ogwe <symonogwe@gmail.com>
2023-09-19 13:54:37 +0300

    Delete opacity and transform from nav-container

--- src/hero.js
+++ src/hero.js
@@ -4,5 +4,5 @@ export const dropDownMenu = document.querySelector(".nav-links-container");
 
 export const toggleMenuBtn = document.querySelector(".toggle-menu");
 toggleMenuBtn.addEventListener("click", () => {
-  navComponents.toggleDropDown(dropDownMenu);
+  navComponents.toggleDropDown(dropDownMenu, navComponents.menuArrow);
 });
