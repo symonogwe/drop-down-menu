7039fd0bb76edba104ccd8e0fb0a956f6b3f04d4
Symon Ogwe <symonogwe@gmail.com>
2023-09-19 21:21:15 +0300

    Create nav.js

--- src/nav.js
+++ src/nav.js
@@ -1,17 +1,15 @@
-import "./styles/styles.css";
-
-// import toggleMenuBtn from "./nav";
-
-const toggleMenuBtn = document.querySelectorAll(".toggle-menu");
-
 function toggleDropDown() {
   const dropDown = document.querySelector(".nav-links-container");
   const caret = document.querySelector(".caret");
 
-  dropDown.classList.toggle(".visible-menu");
+  dropDown.classList.toggle("visible-dropDown");
+
   caret.classList.toggle("caret-rotate");
 }
 
+const toggleMenuBtn = document.querySelectorAll(".toggle-menu");
 toggleMenuBtn.forEach((btn) => {
   btn.addEventListener("click", toggleDropDown);
 });
+
+export default toggleMenuBtn;
