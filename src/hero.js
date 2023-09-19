caf7f15d827e0b45906d6e258e1978cb89e6583a
Symon Ogwe <symonogwe@gmail.com>
2023-09-19 20:27:56 +0300

    Delete drop-down image
    - replace it with caret
    - delete nav.js & hero.js

--- src/hero.js
+++
@@ -1,8 +0,0 @@
-import * as navComponents from "./nav";
-
-export const dropDownMenu = document.querySelector(".nav-links-container");
-
-export const toggleMenuBtn = document.querySelector(".toggle-menu");
-toggleMenuBtn.addEventListener("click", () => {
-  navComponents.toggleDropDown(dropDownMenu, navComponents.menuArrow);
-});
--- src/index.js
+++
@@ -1,2 +0,0 @@
-import "./styles/styles.css";
-import * as heroComponents from "./hero";
